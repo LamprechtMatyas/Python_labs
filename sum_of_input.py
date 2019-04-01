31c4bc6d8edd2c30d8923374502150804d65bb3c
LamprechtMatyas <45575382+LamprechtMatyas@users.noreply.github.com>
2019-04-01 12:46:17 +0200

    Added function

--- sum_of_input.py
+++ sum_of_input.py
@@ -1,9 +1,17 @@
 import sys
 
-num = len(sys.argv)
 
-suma = 0
-for i in range(1, num):
-    suma += int(sys.argv[i])
+def _main():
+    num = len(sys.argv)
+    print(_sum_input(num))
 
-print(suma)
+
+def _sum_input(num: int):
+    suma = 0
+    for i in range(1, num):
+        suma += int(sys.argv[i])
+    return suma
+
+
+if __name__ == "__main__":
+    _main()
